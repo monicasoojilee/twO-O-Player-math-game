9bb13dc5ffae2e1b1105a8b64e877a502f62b4b8
MonajiLee <lee.smonica@gmail.com>
2018-11-06 15:59:03 +0000

    Create Player class and instantiate into game logic

--- math_game/main.rb
+++ math_game/main.rb
@@ -1,49 +1,44 @@
-class GameStart
-    def self.start
-        new_game = GameStart.new
+class Player
+    attr_accessor :lives
+    def initialize
+        @lives = 3
     end
 end
 
-# class Player
-#     attr_accessor :lives, :right_answer, :wrong_answer
-    
-#     def initialize
-#         @lives = 3
-#     end
-
-#     def right_answer
-#         puts "YES! You are correct."
-#     end
-
-#     def wrong_answer
-#         @lives -= 1
-#         puts "Seriously? No!"
-#     end
-# end
-
 class MainGame
-    lives = 3;
-    while (lives > 0)
+    player = []
+    player1 = Player.new
+    player2 = Player.new
+    players = [player1, player2]
+    current_player = 0
+
+    while players[current_player].lives > 0
         number_one = rand(1..20)
         number_two = rand(1..20)
-        correct_answer = number_one + number_two
+        correct_answer = number_one + number_two   
+
+        puts "#{players[current_player]}'s turn!"
         puts "What does #{number_one} + #{number_two} equal?"
         print "> "
         answer = $stdin.gets.chomp.to_i
 
-        if answer == correct_answer
-            puts "YES! You are correct."
-        else
-            lives -= 1
-            puts "Seriously? No!"
+        if players[current_player].lives > 0
+            if answer == correct_answer
+                puts "YES! You are correct."
+                puts players[current_player].lives
+                current_player = (current_player + 1) % 2
+            else
+                players[current_player].lives -= 1
+                puts players[current_player].lives
+                puts "Seriously? No!"
+                current_player = (current_player + 1) % 2
+            end
+        end
+
+        if players[current_player].lives == 0
+            puts "----- GAME OVER -----"
+            puts "#{players[(current_player + 1) % 2]} wins with a score of #{players[(current_player + 1) % 2].lives} / 3"
+            puts "Thank you for playing!"
         end
     end
 end
-
-class GameOver
-    puts "#{Player} wins with a score of #{Player.lives}"
-    puts "----- GAME OVER -----"
-    puts "Good bye!"
-end
-
-GameStart.start
