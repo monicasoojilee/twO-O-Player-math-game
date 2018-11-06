d8e32515544ad40b773d62b118ea9e5e90633068
MonajiLee <lee.smonica@gmail.com>
2018-11-06 16:47:03 +0000

    Debug extra iteration of while loop and replace Player class object id with names

--- math_game/main.rb
+++ math_game/main.rb
@@ -1,44 +1,45 @@
 class Player
-    attr_accessor :lives
-    def initialize
+    attr_accessor :lives, :name
+    def initialize(name)
         @lives = 3
+        @name = name
     end
 end
 
 class MainGame
-    player = []
-    player1 = Player.new
-    player2 = Player.new
+# INSTANTIATE FROM PLAYER CLASS
+    player1 = Player.new("Player 1")
+    player2 = Player.new("Player 2")
+
+# TURN-BASED ROTATION VIA ARRAY INDEX MANIPULATION
     players = [player1, player2]
     current_player = 0
 
+    # MAIN GAME
     while players[current_player].lives > 0
+        
+        #QUESTION && PLAYER PROMPT
         number_one = rand(1..20)
         number_two = rand(1..20)
         correct_answer = number_one + number_two   
 
-        puts "#{players[current_player]}'s turn!"
+        puts "#{players[current_player].name}'s turn!"
         puts "What does #{number_one} + #{number_two} equal?"
         print "> "
         answer = $stdin.gets.chomp.to_i
+        current_player = (current_player + 1) % 2
 
-        if players[current_player].lives > 0
-            if answer == correct_answer
-                puts "YES! You are correct."
-                puts players[current_player].lives
-                current_player = (current_player + 1) % 2
-            else
-                players[current_player].lives -= 1
-                puts players[current_player].lives
+        if answer == correct_answer
+            puts "YES! You are correct."
+        else
+            players[current_player].lives -= 1
+            if players[current_player].lives == 0
+                puts "----- GAME OVER -----"
+                puts "#{players[(current_player + 1) % 2].name} wins with a score of #{players[(current_player + 1) % 2].lives} / 3"
+                puts "Thank you for playing!"
+            else 
                 puts "Seriously? No!"
-                current_player = (current_player + 1) % 2
             end
-        end
-
-        if players[current_player].lives == 0
-            puts "----- GAME OVER -----"
-            puts "#{players[(current_player + 1) % 2]} wins with a score of #{players[(current_player + 1) % 2].lives} / 3"
-            puts "Thank you for playing!"
         end
     end
 end
